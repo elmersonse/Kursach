2a41f298aaf567f01ae70f5fc4d32b3d44bf707f
elmersonse <pkititsa200@gmail.com>
2022-04-18 22:37:46 +0300

    Добавьте файлы проекта.

---
+++ Kursach/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NjE3ODU5QDMyMzAyZTMxMmUzME1pWHI0Qk9HZ0UzTUlrcWtkUVBReWxWMWVHNm1rN01sMnBET09EQkVCWEE9");
+        }
+    }
+}
---
+++ Kursach/TableViewPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для TableViewPage.xaml
+    /// </summary>
+    public partial class TableViewPage : Page
+    {
+        public TableViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
