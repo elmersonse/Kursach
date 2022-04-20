b75ae48637732955dea3847823470ee35649e921
elmersonse <pkititsa200@gmail.com>
2022-04-20 23:21:22 +0300

    table views + date w/o time in datagrid

--- Kursach/TableViewPage.xaml.cs
+++ Kursach/TableViewPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,18 +22,117 @@ namespace Kursach
     /// </summary>
     public partial class TableViewPage : Page
     {
+        private const string _connectionString = "Server=ALEXTRAZA\\SQLEXPRESS;Database=SummerCamp;Trusted_Connection=True;";
+        private SqlConnection _connection;
+        private SqlCommand _command;
+        private SqlDataReader _reader;
+        private DataTable _table;
 
         public TableViewPage()
         {
             InitializeComponent();
             DataContext = new ViewModel();
+            try
+            {
+                _connection = new SqlConnection(_connectionString);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         private void TablesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (TablesListBox.SelectedIndex != -1)
             {
-
+                try
+                {
+                    using (_connection = new SqlConnection(_connectionString))
+                    {
+                        _connection.Open();
+                        _command = new SqlCommand();
+                        _command.Connection = _connection;
+                        _table = new DataTable();
+                        switch (TablesListBox.SelectedIndex)
+                        {
+                            case 0:
+                                _command.CommandText = "select * from Вожатый";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 1:
+                                _command.CommandText = "select * from Запись_в_кружок";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 2:
+                                _command.CommandText = "select * from Комната";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 3:
+                                _command.CommandText = "select * from Корпус";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 4:
+                                _command.CommandText = "select * from Кружок";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 5:
+                                _command.CommandText = "select * from Мероприятие";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 6:
+                                _command.CommandText = "select * from Отряд";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 7:
+                                _command.CommandText = "select * from Ребёнок";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 8:
+                                _command.CommandText = "select * from Смена";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                            case 9:
+                                _command.CommandText = "select * from Участие_в_мероприятии";
+                                _reader = _command.ExecuteReader();
+                                _table.Load(_reader);
+                                DataGrid.ItemsSource = _table.DefaultView;
+                                _reader.Close();
+                                break;
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
         }
 
@@ -40,6 +141,14 @@ namespace Kursach
             if (ViewsListBox.SelectedIndex != -1)
             {
 
+            }
+        }
+
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyType == typeof(System.DateTime))
+            {
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "dd/MM/yyyy";
             }
         }
     }
